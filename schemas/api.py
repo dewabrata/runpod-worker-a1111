545fbb73558bb18cd1157348c02d90aefe809d24
dewabrata <dewabrata@gmail.com>
2023-11-05 23:18:16 +0700

    Update api.py roop

--- schemas/api.py
+++ schemas/api.py
@@ -30,7 +30,8 @@ API_SCHEMA = {
             'controlnet/model_list',
             'sdapi/v1/refresh-checkpoints',
             'sdapi/v1/refresh-loras',
-            'sdapi/v1/interrogate'
+            'sdapi/v1/interrogate',
+            'roop/image'
         ]
     }
 }
